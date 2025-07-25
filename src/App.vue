d769613bca932a862fff8ddd374b44d9ed0ef248
liuxuewen <947752894@qq.com>
2025-07-25 14:27:16 +0800

    add: add info into README.md change: open limit

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,21 @@
 import { onMounted } from 'vue'
 onMounted(() => {
   // 禁止F12、Ctrl+Shift+I、Ctrl+U、Ctrl+S、右键
-  // window.addEventListener('keydown', (e) => {
-  //   if (
-  //     e.key === 'F12' ||
-  //     (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'i') ||
-  //     (e.ctrlKey && e.key.toLowerCase() === 'u') ||
-  //     (e.ctrlKey && e.key.toLowerCase() === 's')
-  //   ) {
-  //     e.preventDefault()
-  //     e.stopPropagation()
-  //     return false
-  //   }
-  // })
-  // window.addEventListener('contextmenu', (e) => {
-  //   e.preventDefault()
-  //   return false
-  // })
+  window.addEventListener('keydown', (e) => {
+    if (
+      e.key === 'F12' ||
+      (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'i') ||
+      (e.ctrlKey && e.key.toLowerCase() === 'u') ||
+      (e.ctrlKey && e.key.toLowerCase() === 's')
+    ) {
+      e.preventDefault()
+      e.stopPropagation()
+      return false
+    }
+  })
+  window.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    return false
+  })
 })
-</script> 
+</script>
